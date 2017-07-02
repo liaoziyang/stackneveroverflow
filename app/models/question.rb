5d17253eae581786c648ce130cf4ec5a891601fd
liaoziyang <liaoziyang0405@gmail.com>
2017-07-03 00:40:30 +0900

    Add some views

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,7 +5,7 @@ class Question < ApplicationRecord
 
   def self.search(search) #self.でクラスメソッドとしている
     if search # Controllerから渡されたパラメータが!= nilの場合は、titleカラムを部分一致検索
-      Question.where(['title LIKE ?', "%#{search}%"])
+      Question.where(['title LIKE ? OR content LIKE ?', "%#{search}%", "%#{search}%"])
     else
       Question.all #全て表示。
     end
