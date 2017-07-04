a1c77906c065e6e292872101237455cb0e6c9b83
liaoziyang <liaoziyang0405@gmail.com>
2017-07-04 15:33:53 +0900

    Show questions in top page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,9 @@
 class HomeController < ApplicationController
   def index
+    @questions = Question.search(params[:search])
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @questions }
+    end
   end
 end
