9abac8c034eb627693f3373c74d58e25096adecc
liaoziyang <liaoziyang0405@gmail.com>
2017-07-06 11:06:33 +0900

    Fix bugs

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :answers, :except => [:index, :show]
-  resources :questions, :except => [:index]
+  resources :questions
   devise_for :users
   get 'home/index'
   root 'home#index'
